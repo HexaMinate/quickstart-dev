fdc24f6222d521fabdc3c9cf2250d3e8c66c0483
Azkadev <azkadev@pop-os.localdomain>
2021-12-06 16:44:35 +0700

    update

--- script.google.com/telegram-bot-welcome/code.js
+++ script.google.com/telegram-bot-welcome/code.js
@@ -14,8 +14,46 @@ function setWebhook() {
     console.log(data);
 }
 
-function fetch(){
-    
+function database(method, parameters = {}) {
+    if (!config_json.token_database) {
+        throw {
+            "message": 'Bot Token is required'
+        };
+    } else {
+        if (String(config_json.token_database).split(":").length == 0) {
+            throw {
+                message: `Format token salah tolong isi dengan benar ya`
+            };
+        }
+    }
+    if (!method) {
+        throw {
+            "message": 'Method is required'
+        };
+    }
+    var options = {
+        'method': 'post',
+        'contentType': 'application/json'
+    };
+    if (parameters) {
+        options['payload'] = JSON.stringify(parameters);
+    }
+    var url = "https://hexaminate.herokuapp.com/database/nosql/api/" + config_json.token_database;
+    var response = UrlFetchApp.fetch(url, options);
+    if (response.getResponseCode() == 200) {
+        if (blob) {
+            return response.getBlob();
+        } else {
+            return JSON.parse(response.getContentText());
+        }
+    }
+    return false;
+
+}
+
+function setDatabaseTelegram() {
+    var data = database("setValue", { "key": "group", "value": [] });
+    console.log(data);
 }
 
 function doPost(e) {
@@ -45,8 +83,7 @@ function doPost(e) {
                     var fromFullName = `${fromFname} ${fromLname}`;
                     var fromUsername = (msg.from.username) ? `@${msg.from.username}` : "";
                     var fromLanguagecode = msg.from.language_code ?? "id";
-                    var mentionFromMarkdown = `[${fromFullName}](tg://user?id=${user_id})`;
-                    var mentionFromHtml = `<a href='tg://user?id=${user_id}'>${fromFullName}</a>`;
+                    var mentionFromMarkdown = "[" + fromFullName + "](tg://user?id=" + user_id + ")";
                     var key = { chat: { id: chat_id } };
 
                     try {
@@ -72,6 +109,25 @@ function doPost(e) {
                                     };
                                     return tg.request("sendMessage", option);
                                 } else {
+                                    var getValue = database("getValue", {
+                                        "key": "group"
+                                    });
+                                    var foundValue = false
+                                    for (var index = 0; index < getValue.length; index++) {
+                                        var loop_data = getValue[index];
+                                        if (loop_data.chat && loop_data.chat.id == chat_id) {
+                                            foundValue = true;
+                                        }
+                                    }
+                                    if (!foundValue) {
+                                        database("pushValue", {
+                                            "key": "group",
+                                            "value": {
+                                                "chat": msg.chat,
+                                                "welcome": false
+                                            }
+                                        });
+                                    }
                                     var option = {
                                         "chat_id": chat_id,
                                         "text": "Hello there my name is bot",
@@ -108,6 +164,33 @@ function doPost(e) {
                             }
 
                         }
+
+                        if (msg.new_chat_members) {
+                            var getValue = database("getValue", {
+                                "key": "group"
+                            });
+                            for (var index = 0; index < getValue.length; index++) {
+                                var loop_data = getValue[index];
+                                if (loop_data.chat && loop_data.chat.id == chat_id && loop_data.welcome) {
+                                    try {
+                                        return sendMessage(tg, update, loop_data.welcome);
+                                    } catch (e) {
+                                        database("updateValue", {
+                                            "key": "group",
+                                            "searchdata": {
+                                                "chat": {
+                                                    "id": chat_id
+                                                }
+                                            },
+                                            "value": {
+                                                "welcome": false
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        }
+
                     } catch (e) {
                         var option = {
                             "chat_id": chat_id,
@@ -120,5 +203,62 @@ function doPost(e) {
         }
     } catch (e) {
 
+    }
+}
+
+function sendMessage(tg, update, getData) {
+
+    if (update && getData) {
+
+        if (update.message) {
+            var msg = update.message;
+            var msgr = msg.reply_to_message ?? false;
+            var user_id = msg.from.id;
+            var chat_id = msg.chat.id;
+            var chat_type = String(msg.chat.type).replace(RegExp("super", "i"), "");
+            var chat_title = msg.chat.title ?? "";
+            var chat_username = (msg.chat.username) ? `@${msg.chat.username}` : "";
+            var msg_id = msg.message_id;
+            var text = msg.text ?? "";
+            var caption = msg.caption ?? "";
+            var fromId = msg.from.id;
+            var fromFname = msg.from.first_name;
+            var fromLname = msg.from.last_name ?? "";
+            var fromFullName = `${fromFname} ${fromLname}`;
+            var fromUsername = (msg.from.username) ? `@${msg.from.username}` : "";
+            var fromLanguagecode = msg.from.language_code ?? "id";
+            var mentionFromMarkdown = "[" + fromFullName + "](tg://user?id=" + user_id + ")";
+            var key = { chat: { id: chat_id } };
+
+            if (getData.type) {
+                var message = "";
+                if (getData.content) {
+                    message += String(getData.content).replace(/({name})/ig, fromFname).replace(/({username})/ig, fromUsername).replace(/({chat_title})/ig, chat_title);
+                }
+
+                if (RegExp("^message|photo|video$", "i").exec(getData.type)) {
+                    var option = {
+                        "chat_id": chat_id,
+                        "allow_sending_without_reply": true,
+                        "reply_to_message_id": msg_id,
+                        "parse_mode": "html"
+                    };
+                    option[String(getData.type).toLocaleLowerCase()] = getData.type;
+                    if (RegExp("^message$", "i").exec(getData.type)) {
+                        option["text"] = message ?? "Hello new member";
+                    } else {
+                        option["caption"] = message ?? "";
+                    }
+                    return tg.request("send" + getData.type, option);
+                }
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+    } else {
+        return false;
     }
 }
